30002081903e9ddefee1cbe31f268be1bf77747b
elrickVM <ecryantube@gmail.com>
2012-10-20 20:17:10 -0400

    Loggin Sorted List Change

--- js/influenceMap.js
+++ js/influenceMap.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function(){
         $(function() {
 		$( "#accordion" )
 			.accordion({
-				header: "div > h3"
+				header: " > div > h3"
 			})
 			.sortable({
 				axis: "y",
@@ -17,22 +17,104 @@ jQuery(document).ready(function(){
 				update: function(e, ui) {
 					var order = $(this).sortable('toArray');
 					console.log(order);
+					//console.log($(this).children());
+					var $children = $(this).children();
+					//console.log($children);
 					console.log("Div " + ui.item.attr("id") + " now at position " + (ui.item.index() + 1).toString());
 					console.log("Position Has changed: Thank You");
+					var test = ($(ui.item).find("a").html());
+					$("#printLog").html("Moved Section: " + test);
+					//$("#printLog").append(order.join("<br />"));
+
+					var count = 0;
+
+					$.each( $children, function( key, value ) {
+						$("#printLog").append($("<li>" + $(value).attr("id") +" "+ $(value).find(":input").val() + " " + value.className + "</li>"));
+						console.log(value + " " + count);
+						count ++;
+					});			
+
+
+
+
+
+
+
 				}
 			});
 	});
+
+        $('input[id^="label"]').focus(function() {
+
+        	var defaultText = this.value;
+        	console.log(defaultText);
+
+        	var labelHolder = $(this).attr("id");
+        	var sectionName = $(this).parents("div").find("a").html();
+        	console.log(sectionName);
+        	var sectionNumber = labelHolder.slice(5);
+
+        	console.log($(this).val());
+        	if ($(this).length > 0) {
+        		console.log("This length is greater than 0");
+        		$(this).val(" ");
+        	}
+
+        	$(this).keyup(function(e) {
+
+        		//$(this).parents("div").find("a").html(e.value);
+        		//$('a[id$="' + sectionNumber + '"]').html(this.value);
+        		//console.log(this);
+        		console.log($(this).closest("div").find("a").html());
+        		$(this).closest("div").find("a").html(this.value);
+
+
+        	}).blur(function() {
+        		console.log($(this).val());
+
+        		if ($(this).val() === " ") {
+
+        			$(this).val(defaultText);
+
+        			console.log("Inside if: " + defaultText);
+        		}
+
+        		console.log(sectionName);
+
+        		console.log("Blur section is active");
+
+        	});
+        	
+        	//
+        });
+
+
+
+
+
+
+
+
+
+
+
+    /*    
 	//dynamicly change section header with accordions first input
 	$('input[id^="label"]').keyup(function() {
 
 		// collect the id of element in focus	
 		var labelHolder = $(this).attr("id");
+		console.log(this);
 		console.log(labelHolder);
 		// trim of the naming convention and keep the trailing number
 		var sectionNumber = labelHolder.slice(5);
+		console.log(sectionNumber);
 		//adds the input value to the section header
 		$('a[id$="' + sectionNumber + '"]').html(this.value);
+		console.log(this.value);
+
 	});
+	*/
 	
  
 	/*
